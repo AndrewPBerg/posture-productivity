72673b29bc96686eff2e29361677c9d1e10658db
AndrewPBerg <andberg9@gmail.com>
2024-06-19 17:08:05 -0400

    replaced all scripts with exec

--- posture-app/play-sound.py
+++
@@ -1,7 +0,0 @@
-# play-sound.pydub
-from pydub import AudioSegment
-from pydub.playback import play
-
-song = AudioSegment.from_wav("bell-notif.wav")
-print("playing sound using  pydub")
-play(song)
